70278a570d9be6352e9c2e5817eb86f972ffcd1f
JULIO CESAR SAENZ CORDERO <52094458+JulixCR@users.noreply.github.com>
2025-12-02 22:29:36 -0600

    Handle anonymous routes in router

--- GestorCuentas/GestorCuentas.UI/Program.cs
+++ GestorCuentas/GestorCuentas.UI/Program.cs
@@ -1,11 +1,35 @@
 using GestorCuentas.UI.Components;
 using GestorCuentas.UI.Services;
+using GestorCuentas.UI.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    })
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/denied";
+        options.SlidingExpiration = true;
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient());
 
 builder.Services.AddScoped<ILoginService, DapperLoginService>();
 
@@ -24,7 +48,43 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapPost("/api/login", async (LoginModel credentials, ILoginService loginService, HttpContext httpContext) =>
+{
+    var areValid = await loginService.ValidateCredentialsAsync(credentials.IdUsuario!, credentials.Clave!);
+
+    if (!areValid)
+    {
+        return Results.Unauthorized();
+    }
+
+    var claimsIdentity = new ClaimsIdentity(
+        [new Claim(ClaimTypes.Name, credentials.IdUsuario!)],
+        CookieAuthenticationDefaults.AuthenticationScheme);
+
+    var authProperties = new AuthenticationProperties
+    {
+        IsPersistent = true,
+        ExpiresUtc = DateTimeOffset.UtcNow.AddHours(8)
+    };
+
+    await httpContext.SignInAsync(
+        CookieAuthenticationDefaults.AuthenticationScheme,
+        new ClaimsPrincipal(claimsIdentity),
+        authProperties);
+
+    return Results.Ok();
+}).AllowAnonymous();
+
+app.MapPost("/api/logout", async (HttpContext httpContext) =>
+{
+    await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    return Results.Ok();
+}).RequireAuthorization();
 
 app.Run();
